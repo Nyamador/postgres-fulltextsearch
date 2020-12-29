ab40f5a819a697cbf9a404917340d2ae9829906d
Nyamador <dselasi4u@gmail.com>
2020-12-29 14:36:13 +0000

    Added view for search results

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from.models import *
+import json
+
+def serialize_data(qs, fields):
+    data = {}
+    obj_dict = {}
+    for obj in qs:
+        key = data[int(obj.id)]
+        for field in fields:
+            obj_dict[field] = qs.field
+        data[key] = obj_dict
+        obj_dict = {}
+    return data
 
 # Create your views here.
+def search_endpoint(request):
+    query = request.GET.get('q')
+    data = Blog.objects.filter(tagline__search=query)
+    response = {"data": serialize_data(query, ['name', 'tagline', 'id'])}
+    return JsonResponse(response)
